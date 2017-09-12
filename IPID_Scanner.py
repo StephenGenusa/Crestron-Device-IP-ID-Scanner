a2c422331a0b75f0945f307a78038bf053e62c57
Stephen Genusa <github@genusa.com>
2017-09-11 22:51:52 -0500

    test code

--- IPID_Scanner.py
+++ IPID_Scanner.py
@@ -1,31 +1,5 @@
 #!/usr/bin/env python
 
-"""
-Copyright © 2017 by Stephen Genusa
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-"""
-
 import argparse
 import socket
 #
@@ -40,6 +14,14 @@ def test_ipid(sock, ipid, num_retries):
         #   "  IP ID {0:#0{1}x}".format(ipid, 4), "was not accepted"
         if data == b"\x02\x00\x04\x00\x00\x00\x1F":
             print "  IP ID {0:#0{1}x}".format(ipid, 4), "is open for business"
+            #msg = "\x05\x00\x05\x00\x00\x02\x03\x00"
+            #sock.sendall(msg)
+            #try:
+            #    while True:
+            #        data = sock.recv(2000)
+            #        print hexdump.hexdump(data)
+            #except Exception, e:
+            #    print ("The exception was: %s" % e)
             return
 
 
